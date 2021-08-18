1a2ec1bbd45b7d83bb2f662570ad4caec7f1c435
Makadelo <morakevin109@gmail.com>
2021-08-18 14:36:58 -0500

    TERMINANDO CRUDS DE PRODUCTO

---
+++ Webkd/Controllers/ProductoCompraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webkd.Controllers
+{
+    public class ProductoCompraController : Controller
+    {
+        // GET: ProductoCompra
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
