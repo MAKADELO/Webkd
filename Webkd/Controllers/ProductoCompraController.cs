6185361e52e4fbbd3efbcfb7cfbe16d792c0e8e6
Makadelo <morakevin109@gmail.com>
2021-08-22 13:34:27 -0500

    terminando cruds de PRODUCTO COMPRA

--- Webkd/Controllers/ProductoCompraController.cs
+++ Webkd/Controllers/ProductoCompraController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webkd.Models;
 
 namespace Webkd.Controllers
 {
@@ -11,7 +12,131 @@ namespace Webkd.Controllers
         // GET: ProductoCompra
         public ActionResult Index()
         {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_compra.ToList());
+            }
+
+        }
+
+        public static int totalCompra(int idcompra)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.compra.Find(idcompra).total;
+            }
+        }
+
+        public static string NombreProducto(int idproducto)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idproducto).nombre;
+            }
+        }
+
+        public ActionResult ListarCompra()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.compra.ToList());
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto_compra producto_Compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto_compra.Add(producto_Compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_compra.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto_compra productocompraEdit = db.producto_compra.Where(a => a.id == id).FirstOrDefault();
+                return View(productocompraEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto_compra productocompraEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldproductocompra = db.producto_compra.Find(productocompraEdit.id);
+                    oldproductocompra.id_compra = productocompraEdit.id_compra;
+                    oldproductocompra.id_producto = productocompraEdit.id_producto;
+                    oldproductocompra.cantidad = productocompraEdit.cantidad;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto_compra producto_compra = db.producto_compra.Find(id);
+                    db.producto_compra.Remove(producto_compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
         }
     }
 }
