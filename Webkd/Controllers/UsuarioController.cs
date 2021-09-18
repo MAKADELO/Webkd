fa74a4fa9845d68859aac70edd3629f43693a698
Makadelo <morakevin109@gmail.com>
2021-09-18 18:28:31 -0500

    CARGA MASIVA CSV

--- Webkd/Controllers/UsuarioController.cs
+++ Webkd/Controllers/UsuarioController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using Webkd.Models;
 using System.Web.Security;
 using System.Text;
-using System.Web.Security;
+using System.IO;
 namespace Webkd.Controllers
 {
     public class UsuarioController : Controller
@@ -185,7 +185,78 @@ namespace Webkd.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
         }
+
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                //string para guardar la ruta
+                string filePath = string.Empty;
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    //guardar el archivo
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newusuario = new usuario
+                            {
+                                nombre = row.Split(';')[0],
+                                apellido = row.Split(';')[1],
+                                email = row.Split(';')[2],
+                                password = row.Split(';')[3],
+                            };
+
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.usuario.Add(newusuario);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
     }
+
+
 }
 
 
